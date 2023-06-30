dbdd45066a9bab556f59a79c8f81045fdcc755b1
ABEL K ANTONY <abelkantony98@gmail.com>
2023-06-30 14:44:16 +0530

    update to asteroids
    asteroids spawn from random direction and moves

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,10 +6,11 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField] Sprite[] sprites;
 
-    public float size = 1f;
+    public float size = 1f; 
+    public float speed = 5f;
     public float minSize = 0.5f;
     public float maxSize = 1.5f;
-
+    public float maxLifeTime = 30f;
     private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody;
 
@@ -24,6 +25,16 @@ public class Asteroid : MonoBehaviour
         _spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
         this.transform.eulerAngles = new Vector3(0f, 0f, Random.value * 360f);
         this.transform.localScale = Vector3.one * this.size;
-        _rigidbody.mass = this.size;
+        _rigidbody.mass = this.size ;
+    }
+
+    public float GetMinSize() { return minSize; }
+    public float GetMaxSize() { return maxSize; }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        _rigidbody.AddForce(direction * this.speed);
+        
+        Destroy(this.gameObject, this.maxLifeTime);
     }
 }
---
+++ Assets/Scripts/AsteroidSpwaner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpwaner : MonoBehaviour
+{
+    public Asteroid asteroidPrefab;
+    public float spwanRate = 2f;
+    public int spwanAmount = 1;
+    public float spwanDistance = 15f;
+    public float trajectoryVariance = 15f;
+    private void Start()
+    {
+        InvokeRepeating(nameof(Spwan),this.spwanRate, this.spwanRate);
+    }
+
+    private void Spwan()
+    {
+        for(int i=0; i<spwanAmount; i++)
+        {
+            Vector3 spwanDirection = Random.insideUnitCircle.normalized * spwanDistance;
+            Vector3 spwanPoint = this.transform.position + spwanDirection;
+
+            float variance = Random.Range(-this.trajectoryVariance, this.trajectoryVariance);
+            Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward);
+
+            Asteroid asteroid = Instantiate(asteroidPrefab, spwanPoint, rotation);
+            asteroid.size = Random.Range(asteroid.GetMinSize(), asteroid.GetMaxSize());
+            asteroid.SetTrajectory(rotation * -spwanDirection);
+        }
+    }
+}
